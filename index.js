2d1bc7aa51d26a0537038122a5c4541e99780367
Antonio Aradaza <antonioaradaza9917@gmail.com>
2024-03-14 16:46:27 +0800

    🔥 refactor: delete root index file

--- index.js
+++
@@ -1,8 +0,0 @@
-import app from './src/app.js';
-
-const HOST = process.env.HOST || 'localhost';
-const PORT = process.env.HOST_PORT || 8000;
-
-app.listen(PORT, HOST, () => {
-  console.log('Listening on %s:%d...', HOST, PORT);
-});

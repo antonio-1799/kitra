1a45501decaf882f2c0f898ee76953e20dadcf81
Antonio Aradaza <antonioaradaza9917@gmail.com>
2024-03-14 16:33:09 +0800

    🔥 fire: remove unused imports

--- src/models/index.js
+++ src/models/index.js
@@ -3,6 +3,9 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+// import fs from 'fs';
+// import path from 'path';
+// import Sequelize from 'sequelize';
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';

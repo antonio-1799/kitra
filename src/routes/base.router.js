f532f9ff4e8d3d40ffcd8bf006d50969037b091b
Antonio Aradaza <antonioaradaza9917@gmail.com>
2024-03-15 09:45:51 +0800

    ♻️ refactor: add users router

--- src/routes/base.router.js
+++ src/routes/base.router.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const geolocationRouter = require('./geolocation.router');
+const usersRouter = require('./users.router');
 
 const baseRouter = express.Router();
 
 // Add routers here
 baseRouter.use('/geolocation', geolocationRouter);
+baseRouter.use('/users', usersRouter);
 
 // export default baseRouter;
 module.exports = baseRouter;

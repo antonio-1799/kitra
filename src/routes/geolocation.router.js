05b147c7f7d57cfbcde1aaa2424448f1609fbf3b
Antonio Aradaza <antonioaradaza9917@gmail.com>
2024-03-18 10:21:50 +0800

    💥 refactor: add auth middleware for geolocation endpoint

--- src/routes/geolocation.router.js
+++ src/routes/geolocation.router.js
@@ -4,13 +4,23 @@ const schemaValidator = require('../middlewares/schema-validator.middleware');
 const {
   geolocationSchema,
 } = require('../utils/validations/geolocation.validation');
+const { withJWTAuthMiddleware } = require('express-kun');
+const { SECRET_KEY } = require('../common/constants');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 const geolocationController = new GeolocationController();
 
 const geolocationRouter = express.Router();
+const protectedGeolocationRouter = withJWTAuthMiddleware(
+  geolocationRouter,
+  SECRET_KEY,
+);
 
-geolocationRouter.post('/', schemaValidator(geolocationSchema), (req, res) =>
-  geolocationController.findAllTreasures(req, res),
+protectedGeolocationRouter.post(
+  '/',
+  schemaValidator(geolocationSchema),
+  authMiddleware(),
+  (req, res) => geolocationController.findAllTreasures(req, res),
 );
 
 module.exports = geolocationRouter;
